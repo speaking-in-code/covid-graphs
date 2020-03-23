b91ee05f177d8bd6634d8042693289f7d6175bd2
speakingincode <brian@speakingincode.net>
2020-03-22 20:00:56 -0700

    Add angular materials themes and components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {GraphsModule} from "./graphs/graphs.module";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     GraphsModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   declarations: [
     AppComponent,
